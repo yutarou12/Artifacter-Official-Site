1ef4114985aa5f8e732c49c49adefc3270e6dc31
yutarou12 <sasaki.yuutarou12@gmail.com>
2026-01-21 12:21:02 +0900

    [change] cssを複数ファイルに変更

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './styles/global.css'
+import './styles/mobile.css'
+import './styles/header.css'
+import './styles/footer.css'
+import './styles/adstyle.css'
 import './styles/build-example.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
